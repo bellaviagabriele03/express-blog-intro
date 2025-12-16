873e104c748680570d393295c66e87927a372286
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-16 14:33:35 +0100

    feat: array dei post DONE

--- app.js
+++ app.js
@@ -8,24 +8,50 @@ const app = express();
 const port = 3000;
 
 
-
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.send("IL MIO SERVER")
 })
 
 
-const posts = [{
-    id:"1",
-    title: "Post-1",
-    content: "Oggi ho creato il mio primo server, in locale e senza l'aiuto di matteoStrap !!"
-    
-}]
+const posts = [
+    {
+        id: "1",
+        title: "Post-1",
+        content: "Oggi ho creato il mio primo server, in locale e senza l'aiuto di matteoStrap !!",
+        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
+
+    },
+    {
+        id: "2",
+        title: "Post-2",
+        content: "se solo avessi avuto BootStrap... senza di lui mi sento perso :(",
+        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
+
+    },
+    {
+        id: "3",
+        title: "Post-3",
+        content: "Da domani npm install Vado a Correre, P.S: non ci credi nemmeno tu !",
+        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david"]
+
+    },
+    {
+        id: "4",
+        title: "Post-4",
+        content: "Miao Corporation: azieda esclusiva per chi usa solo BootStrap e non matteoStrap. Nata il giorno: 404 Error (esiste da sempre : D)",
+        tags: ["local", "host", "matteoStrap", "react è morto", "npm install david", "node ./miao-main.Loris"]
+
+    },
+]
+
+
 
 
 
 app.listen(port, () => {
     console.log(`Server avviato con successo nella porta ${port}`);
-    
+
 })
 
