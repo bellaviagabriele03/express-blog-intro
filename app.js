b834b336e15ffba5dc48ba7aba76290f7c4c945e
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-16 14:22:34 +0100

    feat: primo milestone, immagini

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ console.log("Hello Boolean !!");
 
 import { log } from "console";
 import express from "express"
+import { title } from "process";
 
 const app = express();
 const port = 3000;
 
+
+
+
+app.get("/", (req, res) => {
+    res.send("IL MIO SERVER")
+})
+
+
+const posts = [{
+    id:"1",
+    title: "Post-1",
+    content: "Oggi ho creato il mio primo server, in locale e senza l'aiuto di matteoStrap !!"
+    
+}]
 
 
 
